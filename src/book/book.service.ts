996717e1710ec21a54e946b100338a9343db0b88
yerlo <lineroacostacarlos@gmail.com>
2022-10-10 23:44:11 -0300

    add model class and ts dev dependecy

---
+++ src/book/book.service.ts
@@ -0,0 +1,25 @@
+import { Book } from "./book.model";
+import { Author } from "../author/author.model";
+import { Publisher } from "../publisher/publisher.model";
+
+export class BookService {
+
+  private _bookList: Book[] = [];
+
+  public constructor() {
+    this.fillBookList();
+  }
+
+  public fillBookList() {
+    this._bookList = [
+      new Book('Todo por volver a verte', '', '', new Author('Florencia Vercellone'), 10, new Publisher('Ivrea'),
+        '', 256, 'Español', 'Romance', new Date, 4)
+    ]
+  }
+
+
+  public get bookList(): Book[] {
+    return this._bookList;
+  }
+
+}
---
+++ src/main/index.ts
@@ -0,0 +1,51 @@
+import { Book } from "../book/book.model.js";
+import { BookService } from "../book/book.service.js";
+
+let bookService = new BookService();
+let books: Book[] = bookService.bookList;
+
+// 1. Select the div element using the id property
+const app = document.getElementById("app");
+// 2. Create a new <p></p> element programmatically
+const p = document.createElement("p");
+// 3. Add the text content
+p.textContent = "Hello, World!";
+// 4. Append the p element to the div element
+app?.appendChild(p);
+console.log("");
+
+const recomendedBooks = document.getElementById("recomended-books");
+fillRecomendedBooks();
+
+function fillRecomendedBooks() {
+  books.forEach(book => {
+    let divCol = document.createElement("div");
+    divCol.classList.add("col-3")
+
+    let divRow = document.createElement("div");
+    divRow.classList.add("row");
+
+    let bookCover = document.createElement("img");
+    bookCover.classList.add("img-fluid col-3");
+    bookCover.src = book.img;
+    divRow.appendChild(bookCover);
+
+    let spanScore = document.createElement("span");
+    spanScore.classList.add("col-12");
+    spanScore.textContent = book.score.toString();
+    divRow.appendChild(spanScore);
+
+    let spanTitle = document.createElement("span");
+    spanTitle.classList.add("col-12");
+    spanTitle.textContent = book.title;
+    divRow.appendChild(spanTitle);
+
+    let spanAuthorName = document.createElement("span");
+    spanAuthorName.classList.add("col-12");
+    spanAuthorName.textContent = book.author.name;
+    divRow.appendChild(spanAuthorName);
+
+    divCol.appendChild(divRow);
+    recomendedBooks?.appendChild(divCol);
+  });
+}
