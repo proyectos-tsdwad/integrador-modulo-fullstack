d8a00e40995b11b8ee9a3a71ce6f3c3cefe5904b
carlos linero <lineroacostacarlos@gmail.com>
2022-10-16 01:26:34 -0300

    cosmetic: change variables names and add main Js file

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,53 +1,54 @@
 import { Book } from "./book.model";
 import { Author } from "../author/author.model";
 import { Publisher } from "../publisher/publisher.model";
-import recommendedBooks from '../example-data/recommendedBooks.json';
+import recommendedBooks from '../simulation-data/recommendedBooks.json';
+import incomingBooks from '../simulation-data/incomingBooks.json';
 
 export class BookService {
 
   getRecommended(): Book[] {
 
-    let recommndedBooks = recommendedBooks.map(book => new Book(
+    let books = recommendedBooks.map(book => new Book(
       book.title, book.img, book.description, new Author(book.author.name), book.stock, new Publisher(book.publisher.name),
       book.isbn, book.pageAmount, book.language, book.genre, new Date(book.releaseDate), book.score));
 
-    return recommndedBooks;
+    return books;
   }
 
-  getNewIncomings(): Book[] {
+  getIncomings(): Book[] {
 
-    let incomingBooks = recommendedBooks.map(book => new Book(
+    let books = incomingBooks.map(book => new Book(
       book.title, book.img, book.description, new Author(book.author.name), book.stock, new Publisher(book.publisher.name),
       book.isbn, book.pageAmount, book.language, book.genre, new Date(book.releaseDate), book.score));
 
-    return incomingBooks;
+    return books;
   }
 
   getBestSelling(): Book[] {
 
-    let bestSellingBooks = recommendedBooks.map(book => new Book(
+    let books = recommendedBooks.map(book => new Book(
       book.title, book.img, book.description, new Author(book.author.name), book.stock, new Publisher(book.publisher.name),
       book.isbn, book.pageAmount, book.language, book.genre, new Date(book.releaseDate), book.score));
 
-    return bestSellingBooks;
+    return books;
   }
 
   getByCategory(): Book[] {
 
-    let booksByCategory = recommendedBooks.map(book => new Book(
+    let books = recommendedBooks.map(book => new Book(
       book.title, book.img, book.description, new Author(book.author.name), book.stock, new Publisher(book.publisher.name),
       book.isbn, book.pageAmount, book.language, book.genre, new Date(book.releaseDate), book.score));
 
-    return booksByCategory;
+    return books;
   }
 
   getRentalHistory(): Book[] {
 
-    let rentalHistoryBooks = recommendedBooks.map(book => new Book(
+    let books = recommendedBooks.map(book => new Book(
       book.title, book.img, book.description, new Author(book.author.name), book.stock, new Publisher(book.publisher.name),
       book.isbn, book.pageAmount, book.language, book.genre, new Date(book.releaseDate), book.score));
 
-    return rentalHistoryBooks;
+    return books;
   }
 
 }
--- src/main/main.ts
+++ src/main/main.ts
@@ -8,15 +8,32 @@ const bookRender = new BookRenderService();
 const recomendedBooksElement = document.getElementById("recomended-books");
 createRecomendedBooksView();
 
+const incomingBooksElement = document.getElementById("incoming-books");
+createincomingBooksView();
+
 function createRecomendedBooksView() {
 
-  if(!recomendedBooksElement){
+  if (!recomendedBooksElement) {
     return;
   }
 
   let recommendedBooks: Book[] = bookService.getRecommended();
 
-  const recommendedBooksView = bookRender.createBooksView(recommendedBooks);
+  let recommendedBooksView: HTMLDivElement = bookRender.createBooksView(recommendedBooks);
 
   recomendedBooksElement.appendChild(recommendedBooksView);
+}
+
+
+function createincomingBooksView() {
+
+  if (!incomingBooksElement) {
+    return;
+  }
+
+  let books: Book[] = bookService.getIncomings();
+
+  let incomingBooksView: HTMLDivElement = bookRender.createBooksView(books);
+
+  incomingBooksElement.appendChild(incomingBooksView);
 }
